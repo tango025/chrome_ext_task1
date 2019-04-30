972d27ad662583d480b56c60ef0ce0fadf582376
tango025 <gaurav2550.gg@gmail.com>
2019-05-01 03:38:38 +0530

    partial task 2 finished

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-       one();
-       two();
-        sendResponse({ proto: "hello" });
-    }
-);
+       setTimeout(()=>{one()},1000);
+       setTimeout(()=>{two()},2000);
+        setTimeout(() => { sendResponse({ proto: "hello" })},2000); 
+    return true;
+    });
 var one = ()=>{
     console.log("function one");
 }
--- popup.js
+++ popup.js
@@ -19,20 +19,21 @@ function loadUrls(arr,i,j){
                 //listener for when loading is complete
                 if (changedInfo.status === "complete") {
                     console.log("complete");
-                    chrome.tabs.sendMessage(tabs[0].id, { greeting:"URL LOADED" }, function (response) {
-                        if (response.proto == "hello" && i < arr.length && j < arr[i].length-1){ 
-                            loadUrls(globalArray, i,++j);
+                    chrome.tabs.sendMessage(tabs[0].id, { greeting: "URL LOADED" }, function (response) {
+                        if (response.proto == "hello" && i < arr.length && j < arr[i].length - 1) {
+                            loadUrls(globalArray, i, ++j);
                             return undefined;
-                        }else if(response.proto == "hello" && i < arr.length -1){
-                            loadUrls(globalArray,++i,0);
-                            return undefined;    
-                        }else{
+                        } else if (response.proto == "hello" && i < arr.length - 1) {
+                            loadUrls(globalArray, ++i, 0);
+                            return undefined;
+                        } else {
                             console.log("something false");
                         }
-                });
+                    });
             }});
         });
     });    
 }
    
 });
+
